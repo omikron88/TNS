53a1edf4f93a990fae4fef079ff09bfc387d2fb6
omikron88 <tomas.f.878@gmail.com>
2021-10-28 16:55:37 +0200

    Delete src directory

--- src/machine/ClockTimeoutListener.java
+++
@@ -1,9 +0,0 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package machine;
-
-public interface ClockTimeoutListener {
-    public void clockTimeout();
-}
